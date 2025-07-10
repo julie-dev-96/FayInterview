afe325d7fd415873bb9bc438361ab36c5756a91a
Julie Childress <juliechildress96@gmail.com>
2025-07-10 16:42:54 -0400

    Initial Commit

---
+++ FayInterview/FayInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayInterviewApp.swift
+//  FayInterview
+//
+//  Created by Julie Childress on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FayInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FayInterviewTests/FayInterviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  FayInterviewTests.swift
+//  FayInterviewTests
+//
+//  Created by Julie Childress on 7/10/25.
+//
+
+import Testing
+@testable import FayInterview
+
+struct FayInterviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
